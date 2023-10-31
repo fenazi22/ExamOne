4cbb3c88fb4d90b3d5cc0bc1f751f7bc6447da4c
Faisal Abdullah Alanazi <fenazi22@gmail.com>
2023-10-31 11:08:16 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,77 @@
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        Scanner input=new Scanner(System.in);
+        //---   1    ---
+//        System.out.print("Enter a number: ");
+//        int num1= input.nextInt();
+//        if (num1%2==0){
+//            System.out.println("the number is even");
+//        }else{
+//            System.out.println("the number is odd");
+//        }
+//
+
+
+
+
+
+
+
+        //---   2    ---
+//        boolean exit=true;
+//        int conut=0;
+//    while (true){
+//
+//        System.out.print("Enter number please: ");
+//        int num= input.nextInt();
+//        if (num==-1){
+//            break;
+//        }
+//        conut+=num;
+//        System.out.println("if you want leave input -1");
+//
+//    }
+//        System.out.println("\t------------------\n");
+//        System.out.println("total user input is: "+conut);
+
+
+
+
+
+
+        //---   3    ---
+//int[] arraynum={10, -21 , 30, 31, -25};
+//        for (int i = 0; i <arraynum.length ; i++) {
+//            if (arraynum[i]>0){
+//                System.out.println("\t"+arraynum[i]+" is a positive number  ");
+//            }else {
+//                System.out.println("\t"+arraynum[i]+" is a negative number ");
+//            }
+//        }
+
+
+        //---   4    ---
+
+//
+//String [] cheeck ={"Tuwaiq","Bootcamp","Student","JAVA"};
+//
+//        for (int i = 0; i <cheeck.length ; i++) {
+//            cheeck[i].length();
+//            if (cheeck[i].length()<cheeck[i+1].length()){
+//                int x=cheeck[i].length();
+//                cheeck[i].length()=cheeck[i+1].length();
+//            }
+//        }
+//
+//
+//        System.out.println("\n-------------\n");
+//        for (int i = 0; i <cheeck.length ; i++) {
+//            System.out.println(cheeck[i]);
+//        }
+
+
+    }
+}
